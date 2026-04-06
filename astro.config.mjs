6c50e283a0d71ed72af2ae1e484ee95dee4ffc63
Steve Tapley <steve.tapley5@gmail.com>
2026-04-06 16:46:02 +0800

    sitemap fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -47,7 +47,7 @@ const fontsConfig = Object.entries(theme.fonts.font_family)
 
 // https://astro.build/config
 export default defineConfig({
-  site: config.site.base_url ? config.site.base_url : "http://examplesite.com",
+  site: config.site.base_url ? config.site.base_url : "https://careerclarity.app",
   base: config.site.base_path ? config.site.base_path : "/",
   trailingSlash: config.site.trailing_slash ? "always" : "never",
   image: { service: sharpImageService() },
@@ -55,7 +55,12 @@ export default defineConfig({
   fonts: fontsConfig,
   integrations: [
     react(),
-    sitemap(),
+    sitemap({
+      filter: (page) =>
+        !["/faq", "/pricing", "/blog"].some(
+          (route) => page.endsWith(route) || page.includes(`${route}/`),
+        ),
+    }),
     AutoImport({
       imports: [
         "@/shortcodes/Button",
